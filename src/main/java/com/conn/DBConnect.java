d8a22f1d9eedcd2fb46e8a6741a3063d1ae536d6
chirag <chirag21r>
2025-09-02 00:04:55 +0530

    Credentials for DB conn changed for railway deployment

--- src/main/java/com/conn/DBConnect.java
+++ src/main/java/com/conn/DBConnect.java
@@ -18,9 +18,9 @@ public class DBConnect
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "jdbc:mysql://localhost:3306/votingappj2ee";
+			String url = System.getenv("DB_URL") != null ? System.getenv("DB_URL") : "mysql://root:AVEbllMOfQWBFMpefpQWyZPKIwwBatCK@mysql.railway.internal:3306/railway";
 			String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER") : "root";
-			String pass = System.getenv("DB_PASS") != null ? System.getenv("DB_PASS") : "212004@ChiragShukla";
+			String pass = System.getenv("DB_PASS") != null ? System.getenv("DB_PASS") : "AVEbllMOfQWBFMpefpQWyZPKIwwBatCK";
 			conn = DriverManager.getConnection(url,user,pass);
 
 		
